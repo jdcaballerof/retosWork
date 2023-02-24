47c96f0c94b667149920ac69133eb10c9224b548
David CF <jcaballero@RTP.GOB.MX>
2023-02-24 10:17:26 -0600

    First2

--- myFnd/src/pages/MenuPage.tsx
+++ myFnd/src/pages/MenuPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import { Carrusel } from '../components/Carrusel/Carrusel';
 
@@ -40,9 +40,15 @@ export const MenuPage = () => {
         }));
     }
 
-    let nav:any = document.getElementById('miNavbar');
-    const anchoNav: number = nav.clientWidth
+    let nav:any ;
+    let anchoNav: number
 
+    useEffect(() => {
+      nav = document.getElementById('miNavbar');
+      anchoNav = nav.clientWidth
+      
+    }, [])
+    
 
 
   return (
@@ -51,26 +57,41 @@ export const MenuPage = () => {
       <p  className='text-xl txt' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore corrupti, a necessitatibus porro voluptatem, impedit corporis iste facere dolorem labore tempore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium molestias autem eius quod inventore, excepturi sit quae, tempora omnis saepe assumenda aspernatur est in vel, aliquam aliquid eos quidem consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ab vero itaque deleniti tenetur repellendus illum, voluptatum earum neque inventore, veniam debitis mollitia reprehenderit? Quas modi quod nobis consequatur aut! Laudantium fugit quas nostrum incidunt rem ab repellat fugiat. Dolores numquam at hic excepturi deserunt sit eligendi corporis, illum exercitationem delectus id, pariatur, laborum dolor reprehenderit nihil impedit? Nobis, reiciendis vero!</p>
   
       <nav id='miNavbar' className="fixed inset-x-0 bottom-0 bg-lime-400 nav1">
-        <ul className="flex flex-row  min-h-fit gap-7 items-center w-max">
+        <ul className="flex flex-row  min-h-fit pb-0 gap-7 justify-evenly lg:justify-center items-center min-w-full w-max">
+
           <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Test 1</Link></li>
+          {/* <li> <a onClick={() => scroll.scrollTo(100)}>Scroll To 100!</a></li>
+          <li> <a onClick={() => scroll.scrollToBottom()}>Scroll To Bottom</a></li>
+          <li> <a onClick={() => scroll.scrollMore(2)}>Scroll 2 More!</a></li> */}
           <li><Link activeClass="active" className="test2" to="test2" spy={true} smooth={true} duration={500}>Test 2</Link></li>
+          {/* <li> <a onClick={() => scroll.scrollTo(100)}>Scroll To 100!</a></li>
+          <li> <a onClick={() => scroll.scrollToBottom()}>Scroll To Bottom</a></li>
+          <li> <a onClick={() => scroll.scrollMore(2)}>Scroll 2 More!</a></li> */}
           <li><Link activeClass="active" className="test3" to="test3" spy={true} smooth={true} duration={500} >Test 3</Link></li>
+          {/* <li> <a onClick={() => scroll.scrollTo(100)}>Scroll To 100!</a></li>
+          <li> <a onClick={() => scroll.scrollToBottom()}>Scroll To Bottom</a></li>
+          <li> <a onClick={() => scroll.scrollMore(2)}>Scroll 2 More!</a></li> */}
           <li><Link activeClass="active" className="test4" to="test4" spy={true} smooth={true} duration={500}>Test 4</Link></li>
+          {/* <li> <a onClick={() => scroll.scrollTo(100)}>Scroll To 100!</a></li>
+          <li> <a onClick={() => scroll.scrollToBottom()}>Scroll To Bottom</a></li>
+          <li> <a onClick={() => scroll.scrollMore(2)}>Scroll 2 More!</a></li> */}
           <li><Link activeClass="active" className="test5" to="test5" spy={true} smooth={true} duration={500} delay={1000}>Test 5 ( delay )</Link></li>
           <li><Link activeClass="active" className="test6" to="anchor" spy={true} smooth={true} duration={500}>Test 6 (anchor)</Link></li>
-          <li> <a onClick={() => scroll.scrollTo(100)}>Scroll To 100!</a></li>
-          <li> <a onClick={() => scroll.scrollToBottom()}>Scroll To Bottom</a></li>
-          <li> <a onClick={() => scroll.scrollMore(2)}>Scroll 2 More!</a></li>
-          <li> <a onClick={() => scroll.scrollMore(1000, { delay: 1500 })}>Scroll 1000 More! ( delay ) </a></li>
+
+          <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Test 1</Link></li>
+          <li><Link activeClass="active" className="test5" to="test5" spy={true} smooth={true} duration={500} delay={1000}>Test 5 ( delay )</Link></li>
+          <li><Link activeClass="active" className="test6" to="anchor" spy={true} smooth={true} duration={500}>Test 6 (anchor)</Link></li>
+          {/* <li> <a onClick={() => scroll.scrollMore(1000, { delay: 1500 })}>Scroll 1000 More! ( delay ) </a></li>
           <li><Link activeClass="active" className="test8" to="same" spy={true} smooth={true} duration={500}>Same target</Link></li>
           <li><Link activeClass="active" className="test9" to="same" spy={true} smooth={true} duration={500}>Same target</Link></li>
           <li><a className="" href="test1" onClick={() => scrollTo()} >Scroll to element</a></li>
-          <li><Link className="test1" to="test1" onClick={() => scrollToWithContainer()} >Scroll to element within container</Link></li>
-        </ul>
-        <button className='btn' onClick={() => nav.scroll(anchoNav, 0)}> + </button>
-        <button className='btn2' onClick={() => nav.scroll(0, 0)}> - </button>
+          <li><Link className="test1" to="test1" onClick={() => scrollToWithContainer()} >Scroll to element within container</Link></li> */}
 
+        </ul>
       </nav>
+
+        {/* <button className='btn' onClick={() => nav.scroll(anchoNav, 0)}> + </button>
+        <button className='btn2' onClick={() => nav.scroll(0, 0)}> - </button> */}
       
 
         <Element name="test1" className="element" >
